87bc1854b21a1e085971c4a0fe06c46d6d4228c2
Gabriel Nkrumah-Antiedu <gabdevnkrumah@gmail.com>
2025-11-09 02:24:48 -0800

    Fix: align template paths and view definitions to ALX checker requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import DetailView
 from .models import Book, Library
 
-# Function-based view: list all books
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-from django.views.generic import DetailView
-
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'

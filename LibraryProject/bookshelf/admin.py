3aea2729e13c6ec18b2b835dbbcdb48145981e59
Gabriel Nkrumah-Antiedu <gabdevnkrumah@gmail.com>
2025-11-09 01:19:46 -0800

    Delete LibraryProject directory

--- LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-# Basic registration
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')   # columns visible in admin list view
-    search_fields = ('title', 'author')                      # search bar for title or author
-    list_filter = ('publication_year',)                      # sidebar filter for publication year

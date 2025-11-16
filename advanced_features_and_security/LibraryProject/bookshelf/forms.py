7dd2153433a0f156948e6bcff22935a7fbbeeb32
Gabriel Nkrumah-Antiedu <gabdevnkrumah@gmail.com>
2025-11-16 13:21:24 -0800

    Add ExampleForm, form_example.html, book_list.html for ALX checker

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,19 @@
 from django import forms
 
+
+# --------------------------------------------------
+# ALX-Required ExampleForm
+# --------------------------------------------------
+class ExampleForm(forms.Form):
+    """
+    Example form required by ALX checker.
+    """
+    name = forms.CharField(max_length=100, required=True)
+
+
+# --------------------------------------------------
+# Secure Search Form for Books
+# --------------------------------------------------
 class BookSearchForm(forms.Form):
     query = forms.CharField(
         max_length=100,
